a30a9bbcd9a9d6125504943e8de80f977c1152d5
Tony Thomas <tonythomas504@gmail.com>
2020-12-19 16:13:32 -0500

    committing beginning of Auth code to Auth branch. set up initial typescript variables for login form

--- src/Components/Auth/Auth.tsx
+++ src/Components/Auth/Auth.tsx
@@ -1,1 +1,24 @@
 import React from 'react'
+
+type AuthState = {
+    username: string,
+    password: string;
+    isButtonDisabled: boolean;
+    helperText: string;
+    isError: boolean;
+}
+
+const intialState:AuthState = {
+    username: '',
+    password: '',
+    isButtonDisabled: true,
+    helperText: '',
+    isError: false,
+} 
+
+type Action = 
+{type: 'setusername', payload: string} |
+{type: 'setPassword', payload: string} |
+{type: 'setIsButtonDisabled', payload: string} | {type: 'loginSuccess', payload: string} |
+{type: 'loginFailed', payload: string} |
+{type: 'setIsError', payload: boolean};
