ffe63fa168f3eb6844ae6c138b0b8964f5cb54ad
Tony Thomas <tonythomas504@gmail.com>
2021-01-15 09:25:57 -0500

    could not get material ui navbar to work went with html nav tag

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -7,17 +7,18 @@ import Button from '@material-ui/core/Button';
 import IconButton from '@material-ui/core/IconButton';
 
 
-const useStyles = makeStyles((theme) => ({
-    root: {
-        flexGrow: 1,
-    },
-    menuButton: {
-        marginRight: theme.spacing(2),
-    },
-    title: {
-        flexGrow: 1,
-    },
-}));
+
+// const styles = (theme: any) => ({
+//     root: {
+//         flexGrow: 1,
+//     },
+//     menuButton: {
+//         marginRight: theme.spacing(2),
+//     },
+//     title: {
+//         flexGrow: 1,
+//     },
+// });
 
 type Props = {
     clearToken: () => void;
@@ -25,21 +26,16 @@ type Props = {
 
 export default class NavBar extends React.Component<Props> {
 
-    classes = useStyles()
+
+
 
     render() {
         return (
-            <div className={this.classes.root}>
-                <AppBar position="static">
-                    <Toolbar>
-                        <IconButton edge="start" className={this.classes.menuButton} color="inherit" aria-label="menu">
-                        </IconButton>
-                        <Typography variant="h6" className={this.classes.title}>
-                            News
-          </Typography>
-                        <Button onClick={this.props.clearToken} color="inherit">Logout</Button>
-                    </Toolbar>
-                </AppBar>
+            <div>
+                <nav>
+                    <Button onClick={this.props.clearToken} color="inherit">Logout</Button>
+                </nav>
+
             </div>
         );
     }
